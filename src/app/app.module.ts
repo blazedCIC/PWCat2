c9a085dbabe10469bc354e3927a2f5c497d6c990
David Blaise <dblaise@checkintocash.com>
2018-09-25 08:07:49 -0400

    Upload project to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule
+} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
